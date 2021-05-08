f83c9c73f20cf08edd2423012cd4952fab6d65f8
erfan <erfanmohamadi@hotmail.com>
2021-05-08 19:52:37 +0430

    viewsets

--- shop_admin/urls.py
+++ shop_admin/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet
+from .views import CategoryViewSet,ShopViewSet,SubCategoryViewSet,ProductsViewSet,ProductVariationViewSet
 
 router = DefaultRouter()
 router.register(r'category', CategoryViewSet)
+router.register(r'subcategory', SubCategoryViewSet)
+
+router.register(r'shop', ShopViewSet)
+router.register(r'product', ProductsViewSet)
+router.register(r'variation', ProductVariationViewSet)
+
 urlpatterns = router.urls
 # urlpatterns = +[
 urlpatterns += [
