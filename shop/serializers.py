cfadde80493b36f13cac944935d875d2db8ed303
erfan <erfanmohamadi@hotmail.com>
2021-06-06 12:16:29 +0430

    product

--- shop/serializers.py
+++ shop/serializers.py
@@ -5,7 +5,7 @@ from django.db.models.functions import Coalesce, Least
 from django.utils.datetime_safe import date, datetime
 from rest_framework import serializers
 from rest_framework.exceptions import MethodNotAllowed, ParseError, NotFound
-from shop.models import Shop, Product, ProductVariation, ProductVariationAttribute, ProductAttribute, \
+from shop.models import FrameColor, GlassColor, Shop, Product, ProductVariation, ProductVariationAttribute, ProductAttribute, \
     ProductGalleryImage, ProductReview, DiscountCode, PromotionalCode,WorkingTime,OrderedItem,Transactions,Invoice,TestInPlace,Category,Subcategory
 
 
@@ -83,9 +83,21 @@ class ProductVariationAttributeSerializer(serializers.ModelSerializer):
         model=ProductVariationAttribute
         fields=['id','attribute','attribute_value'] #'attribute','attribute_value'
 
+
+class FrameColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=FrameColor
+        fields = '__all__'
+
+class GlassColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=GlassColor
+        fields = '__all__'
 class VariationPriceSerializer(serializers.ModelSerializer):
     # specifications=ProductAttributeSerializer(many=True,read_only=True)
     specifications=ProductVariationAttributeSerializer(source='productvariationattribute_set', many=True)
+    color=FrameColorSerializer()
+
     #IT IS IMPORTANT TO USE RELATED_NAME in models AND SOURCE in serializer . beCuz Calling the serializer for a ManyToManyField only works on the end point (i.e. ProductAttribute in this case).
     #link: 1-problem:https://www.reddit.com/r/django/comments/6yrh9k/drf_serialization_not_working_on_many_to_many/
     #link: solution:https://www.reddit.com/r/django/comments/6yrh9k/drf_serialization_not_working_on_many_to_many/dms01dv/
@@ -93,7 +105,7 @@ class VariationPriceSerializer(serializers.ModelSerializer):
     # discount_price=serializers.ReadOnlyField()
     class Meta:
         model = ProductVariation
-        fields =["id","title_size","price","discount_price","occasional_discount","specifications","created_at","updated_at"]
+        fields =["id","title_size","price","discount_price","occasional_discount","specifications","created_at","updated_at",'color']
 
 
 
