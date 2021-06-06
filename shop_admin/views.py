e5190f03ad3ddd4e72e16ff03f7cdf48fc00be3d
erfan <erfanmohamadi@hotmail.com>
2021-06-06 13:36:13 +0430

    list product admin viewset

--- shop_admin/views.py
+++ shop_admin/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from shop import models, serializers
 from shop_admin import serializers as admin_serializers
 # Create your views here.
@@ -17,7 +18,16 @@ class ProductsViewSet(viewsets.ModelViewSet):
     """ViewSet for the OrderItem class"""
     queryset = models.Product.objects.all()
     serializer_class = admin_serializers.ProductAdminSerializer
+    def list(self,request):
+        queryset = models.Product.objects.all()
 
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = serializers.ProductDetailSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = serializers.ProductDetailSerializer(queryset, many=True)
+        return Response(serializer.data)
 # class ProductVariationViewSet(viewsets.ModelViewSet):
 #     """ViewSet for the OrderItem class"""
 #     queryset = models.ProductVariation.objects.all()
